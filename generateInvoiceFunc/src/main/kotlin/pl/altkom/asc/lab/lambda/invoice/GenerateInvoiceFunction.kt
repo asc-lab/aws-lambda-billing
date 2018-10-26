e6d52cb8b1682cc08e43ef0708e3be967ac119d8
khelman <kamil.helman@altkom.pl>
2018-10-26 11:52:45 +0200

    Add generateInvoiceFunction

---
+++ generateInvoiceFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/invoice/GenerateInvoiceFunction.kt
@@ -0,0 +1,38 @@
+package pl.altkom.asc.lab.lambda.invoice
+
+import com.amazonaws.services.lambda.runtime.events.SQSEvent
+import org.slf4j.LoggerFactory
+import pl.altkom.asc.lab.lambda.invoice.billing.BillingItem
+import pl.altkom.asc.lab.lambda.invoice.billing.BillingItemRepository
+import pl.altkom.asc.lab.lambda.invoice.invoicing.InvoiceGenerationRequest
+import pl.altkom.asc.lab.lambda.invoice.invoicing.InvoiceGenerator
+import pl.altkom.asc.lab.lambda.invoice.printing.InvoicePrintRequest
+import pl.altkom.asc.lab.lambda.invoice.printing.InvoicePrintRequestPublisher
+import java.util.function.Function
+
+class GenerateInvoiceFunction(
+        val billingItemRepository: BillingItemRepository = BillingItemRepository(),
+        val invoiceGenerator: InvoiceGenerator = InvoiceGenerator(),
+        val invoicePrintRequestPublisher: InvoicePrintRequestPublisher = InvoicePrintRequestPublisher()
+) : Function<SQSEvent, String> {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
+
+    override fun apply(event: SQSEvent): String {
+
+        event.records.forEach(this::processMessage)
+
+        return "OK"
+    }
+
+    private fun processMessage(message: SQSEvent.SQSMessage) {
+        val request = InvoiceGenerationRequest.fromJSON(message.body)
+        val items = billingItemRepository.findForInvoice(request)
+
+        items.map(BillingItem::toString).forEach(log::info)
+
+        val invoice = invoiceGenerator.generate(request, items)
+        invoicePrintRequestPublisher.publish(InvoicePrintRequest(invoice))
+
+    }
+}
---
+++ generateInvoiceFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/invoice/printing/InvoicePrintRequestPublisher.kt
@@ -0,0 +1,16 @@
+package pl.altkom.asc.lab.lambda.invoice.printing
+
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.amazonaws.services.sqs.model.SendMessageRequest
+import com.amazonaws.util.json.Jackson
+
+class InvoicePrintRequestPublisher {
+
+    private val sqs = AmazonSQSClientBuilder.defaultClient()
+    private val queueName: String = System.getenv("INVOICE_PRINT_REQUEST_QUEUE") ?: ""
+    private val queueUrl = sqs.getQueueUrl(queueName).queueUrl
+
+    fun publish(invoicePrintRequest: InvoicePrintRequest) {
+        sqs.sendMessage(SendMessageRequest(queueUrl, Jackson.toJsonPrettyString(invoicePrintRequest)))
+    }
+}
