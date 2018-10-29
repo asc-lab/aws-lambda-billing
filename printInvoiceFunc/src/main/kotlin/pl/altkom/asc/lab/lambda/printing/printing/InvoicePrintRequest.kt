96a7f96d16f005e332f37573166379fa04ca1ef8
khelman <kamil.helman@altkom.pl>
2018-10-29 12:13:28 +0100

    Add more time logging to printInvoiceFunction

--- printInvoiceFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/printing/printing/InvoicePrintRequest.kt
+++ printInvoiceFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/printing/printing/InvoicePrintRequest.kt
@@ -1,11 +1,22 @@
 package pl.altkom.asc.lab.lambda.printing.printing
 
 import com.amazonaws.util.json.Jackson
+import org.slf4j.LoggerFactory
 import pl.altkom.asc.lab.lambda.printing.invoicing.Invoice
 
 data class InvoicePrintRequest(val invoice: Invoice? = null) {
     companion object {
-        fun fromJSON(json: String): InvoicePrintRequest = Jackson.fromJsonString(json, InvoicePrintRequest::class.java)
+        private val log = LoggerFactory.getLogger(InvoicePrintRequest.javaClass)!!
+
+        fun fromJSON(json: String): InvoicePrintRequest {
+            val start = System.currentTimeMillis()
+
+            val result = Jackson.fromJsonString(json, InvoicePrintRequest::class.java)
+
+            log.info("JSON deserialized in ${System.currentTimeMillis()-start}ms")
+
+            return result
+        }
     }
 }
 
--- printInvoiceFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/printing/printing/InvoiceStore.kt
+++ printInvoiceFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/printing/printing/InvoiceStore.kt
@@ -2,15 +2,21 @@ package pl.altkom.asc.lab.lambda.printing.printing
 
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
+import org.slf4j.LoggerFactory
 import java.io.InputStream
 
 class InvoiceStore {
 
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
+
     private val s3Client = AmazonS3Client.builder().build()
     private val bucketName: String = System.getenv("INVOICE_PRINTOUT_BUCKET") ?: ""
 
-
     fun store(stream: InputStream, name: String) {
+        val start = System.currentTimeMillis()
+
         s3Client.putObject(bucketName, name, stream, ObjectMetadata())
+
+        log.info("PDF stored in ${System.currentTimeMillis()-start}ms")
     }
 }
