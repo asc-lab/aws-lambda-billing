e2364ce47a34f6a3867d3cb6db465da5470ad047
khelman <kamil.helman@altkom.pl>
2018-10-30 10:06:22 +0100

    Add more time logging to generateBillingItemFunc

--- generateBillingItemFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/billing/invoicing/InvoiceGenerationRequestPublisher.kt
+++ generateBillingItemFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/billing/invoicing/InvoiceGenerationRequestPublisher.kt
@@ -3,14 +3,22 @@ package pl.altkom.asc.lab.lambda.billing.invoicing
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.model.SendMessageRequest
 import com.amazonaws.util.json.Jackson
+import org.slf4j.LoggerFactory
+import kotlin.system.measureTimeMillis
 
 class InvoiceGenerationRequestPublisher(private val sqs: AmazonSQS) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
 
     private val queueName: String = System.getenv("INVOICE_GENERATION_REQUEST_QUEUE") ?: ""
     private val queueUrl = sqs.getQueueUrl(queueName).queueUrl
 
     fun publish(invoiceGenerationRequest: InvoiceGenerationRequest) {
-        sqs.sendMessage(SendMessageRequest(queueUrl, Jackson.toJsonPrettyString(invoiceGenerationRequest)))
+        val time = measureTimeMillis {
+            sqs.sendMessage(SendMessageRequest(queueUrl, Jackson.toJsonPrettyString(invoiceGenerationRequest)))
+        }
+
+        log.info("InvoiceGenerationRequest published in $time ms")
     }
 
 }
