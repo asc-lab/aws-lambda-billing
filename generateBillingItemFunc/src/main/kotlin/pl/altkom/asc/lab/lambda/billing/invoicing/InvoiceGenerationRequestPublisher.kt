fffc308c4d3c159a5ba33865db42d0dfec100393
khelman <kamil.helman@altkom.pl>
2018-11-06 10:52:08 +0100

    Add time logging to generateBillingItemFunc

--- generateBillingItemFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/billing/invoicing/InvoiceGenerationRequestPublisher.kt
+++ generateBillingItemFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/billing/invoicing/InvoiceGenerationRequestPublisher.kt
@@ -3,16 +3,24 @@ package pl.altkom.asc.lab.lambda.billing.invoicing
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.model.SendMessageRequest
 import com.amazonaws.util.json.Jackson
+import org.slf4j.LoggerFactory
 import javax.inject.Singleton
+import kotlin.system.measureTimeMillis
 
 @Singleton
 class InvoiceGenerationRequestPublisher(private val sqs: AmazonSQS) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
 
     private val queueName: String = System.getenv("INVOICE_GENERATION_REQUEST_QUEUE") ?: ""
     private val queueUrl = sqs.getQueueUrl(queueName).queueUrl
 
     fun publish(invoiceGenerationRequest: InvoiceGenerationRequest) {
-        sqs.sendMessage(SendMessageRequest(queueUrl, Jackson.toJsonPrettyString(invoiceGenerationRequest)))
+        val time = measureTimeMillis {
+            sqs.sendMessage(SendMessageRequest(queueUrl, Jackson.toJsonPrettyString(invoiceGenerationRequest)))
+        }
+
+        log.info("InvoiceGenerationRequest published in $time ms")
     }
 
 }
--- generateBillingItemFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/billing/pricing/PriceListRepository.kt
+++ generateBillingItemFunc/src/main/kotlin/pl/altkom/asc/lab/lambda/billing/pricing/PriceListRepository.kt
@@ -2,15 +2,23 @@ package pl.altkom.asc.lab.lambda.billing.pricing
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
+import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 @Singleton
 class PriceListRepository {
 
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
+
     private val client = AmazonDynamoDBClientBuilder.standard().build()
     private val mapper = DynamoDBMapper(client)
 
     fun get(customerCode: String): PriceList {
-        return mapper.load(PriceList::class.java, customerCode)
+        val start = System.currentTimeMillis()
+        val priceList = mapper.load(PriceList::class.java, customerCode)
+
+        log.info("PriceList fetched in ${System.currentTimeMillis() - start}ms")
+
+        return priceList
     }
 }
